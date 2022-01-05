b2df7fb867aaff64b0f2bb1cb56f591d8465d0e0
tariye-i <97129359+tariye-i@users.noreply.github.com>
2022-01-05 16:43:02 -0500

    Add files via upload

---
+++ Unit 2 classwork 1.py
@@ -0,0 +1,63 @@
+# Unit 2 classwork 1
+
+# 1
+list1 = [1,2,3,4,5,6,7]
+list2 = [2,4,6,8]
+list3 = [1,2,4,5,7]
+
+# 2
+
+def function(anylist):
+    print(anylist)
+
+# 3
+
+def Function(anylist):
+    count = 0
+    newlist = []
+    while (count<len(anylist)):
+        new = anylist[count]+7
+        newlist.append(new)
+        count = count + 1
+    print(newlist)
+
+# 4
+
+def function1(anylist):
+    count = 0
+    smallest_num = anylist[0]
+    while (count<len(anylist)):
+        if (anylist[count]<smallest_num):
+            smallest_num = anylist[count]            
+        count = count + 1
+    print(smallest_num)
+
+# 5
+
+def function2():
+    done = False
+    while (done == False):
+        print ("hey user! what's your favourite food")
+        food = input()
+        if (food == "banana"):
+            done = True
+            print ("goodjob")
+        else:
+            done = False
+
+# 6
+
+def function3():
+    done = False
+    while (done == False):
+        print("hey user! what's your lucky number")
+        luckynum = int(input())
+        if (luckynum == 7):
+            done = True
+            print("okay, bye")
+        else:
+            done = False
+
+
+
+
