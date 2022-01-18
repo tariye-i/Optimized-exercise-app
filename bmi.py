e72d392037e8f82020fb1a9dafb2383e21855728
Taremz <97131407+Taremz@users.noreply.github.com>
2022-01-18 19:00:43 +0100

    Update bmi.py
    I didn't know you couldn't see the changes , so this is a new one

--- bmi.py
+++ bmi.py
@@ -8,16 +8,71 @@ def calculate_bmi():
     bmi = round(bmi, 1)
     bmi_index(bmi)
 
+def bmi_index(bmi):
+    
+    if bmi < 18.5:
+        messagebox.showinfo('bmi-', f'BMI = {bmi} is Underweight')
+    elif (bmi > 18.5) and (bmi < 24.9):
+        messagebox.showinfo('bmi-', f'BMI = {bmi} is Normal')
+    elif (bmi > 24.9) and (bmi < 29.9):
+        messagebox.showinfo('bmi-computerscience', f'BMI = {bmi} is Overweight')
+    elif (bmi > 29.9):
+        messagebox.showinfo('bmi-computerscience', f'BMI = {bmi} is Obese') 
+    else:
+        messagebox.showerror('bmi-computerscience', 'something went wrong!')   
+        
+ws = Tk()
+ws.title('bmi-computerscience')
+ws.geometry('400x300')
+ws.config(bg='#686e70')
 
-if BMI <= 18.5:
-    print("underweight.")
-elif BMI <= 24.9:
-    print(" healthy.")
-elif BMI <= 29.9:
-    print("over weight.")
-elif BMI <= 34.9:
-    print("severely over weight.")
-elif BMI <= 39.9:
-    print(" obese.")
-else:
-    print(" severely obese.")
+var = IntVar()
+
+
+height_lb.grid(row=3, column=1)
+
+weight_lb = Label(
+    frame,
+    text="Enter Weight (kg)  ",
+
+)
+weight_lb.grid(row=4, column=1)
+
+height_tf = Entry(
+    frame,
+)
+height_tf.grid(row=3, column=2, pady=5)
+
+weight_tf = Entry(
+    frame,
+)
+weight_tf.grid(row=4, column=2, pady=5)
+
+frame3 = Frame(
+    frame
+)
+frame3.grid(row=5, columnspan=3, pady=10)
+
+cal_btn = Button(
+    frame3,
+    text='Calculate',
+    command=calculate_bmi
+)
+cal_btn.pack(side=LEFT)
+
+reset_button = Button(
+    frame3,
+    text='Reset',
+    command=reset_entry
+)
+reset_button.pack(side=LEFT)
+
+exit_button = Button(
+    frame3,
+    text='Exit',
+    command=lambda:ws.destroy()
+)
+exit_button.pack(side=RIGHT)
+
+ws.mainloop()
+
