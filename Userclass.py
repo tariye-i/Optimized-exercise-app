2b973403b81cb4104e5dbae2893b3b222240d921
tariye-i <97129359+tariye-i@users.noreply.github.com>
2022-01-18 16:41:57 -0500

    Final changes

--- Userclass.py
+++ Userclass.py
@@ -1,9 +1,8 @@
 class User:
 
-    def __init__(self,name,username,password,age,height,weight):
+    def __init__(self,name,username,password,age):
         self.name = name
         self.username = username
         self.password = password
         self.age = age
-        self.height=height
-        self.weight=weight
+
--- dashboard.py
+++ dashboard.py
@@ -1,32 +1,35 @@
-import usersignup
+import Singup
 import login
-import minisignup
-import bmi
-print('hello user welcome to optimized excercise app')
-print('Do you have an account with us? pls login, or sign up to create an account')
-respon= input()
-if(respon =='yes'):
-    print('That is very nice user, pls what is your username')
-    username=input()
-    print('what is the password given to this specific username')
-    password=input()
-    verification= login.logins(username,password)
-    if(verification==True):
-        print('you may now proceed to dashboard')
+import healthdashboard
+
+donee = False
+while (donee == False):
+    print('hello user welcome to optimized excercise app')
+    print('Do you have an account with us? pls login, or sign up to create an account')
+    respon= input()
+    if(respon =='yes'):
+        print('That is very nice user, pls what is your username')
+        username=input()
+        print('what is the password given to this specific username')
+        password=input()
+        verification= login.logins(username,password)
+        if(verification==True):
+            print('you may now proceed to dashboard')
+            donee = True
+        else:
+            print('your log in details are incorrect, please try again')
+    elif(respon == 'no'):
+        print('would you like to create an account with us?')
+        respon=input()
+        if(respon=='yes'):
+            Singup.create_user()
+            print('congratulations youre almost done. Just one more step')
+            healthdashboard.healthdata_signup()
+            print('congratulations I see that you have made an account with us')
+            print('Please proceed to the bmi tab to calculate your bmi')
+            donee = True
+        else:
+            print('okay, goodbye')
+            donee=True
     else:
-        
-        print('are you sure your details are correct,type yes to create one')
-else:
-    done = False
-    count=0
-    accountbase=[]
-    databasefile=open('database.txt','a')
-    print('would you like to create an account with us')
-    respon=input()
-    if(respon=='yes'):
-        usersignup.classer()
-        print('congratulations i see that you have made an account with us ')
-        minisignup.create_user()
-    else:
-        print('okay then')
-        done=True
+        print('your response is invalid, please input a valid response')
